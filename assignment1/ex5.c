d74dbb31a8e06f3c30bb14b034cd620ea43d850d
Parand <p.mohri@student.maastrichtuniversity.nl>
2023-01-10 15:40:08 +0100

    last

---
+++ assignment1/ex5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//
+// Created by Parand on 18/11/2022.
+//
+// Constant
+#define PI 3.14156
+// "Function"
+#define max(n,m) ((n>m) ? n : m)
+// in main
+#if defined(PI)
+printf(PI);
+#else
+#endif
+//    printf("max(3,4) = %i\n", max(3,4));
